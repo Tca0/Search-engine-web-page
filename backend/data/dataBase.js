99812f6e3e8b7756a3f1c74a7d2be72bf35f76f3
Ami <ami1996_tm@live.co.uk>
2022-09-27 19:20:32 +0100

    Added some test data

--- backend/data/dataBase.js
+++ backend/data/dataBase.js
@@ -1,2 +1,41 @@
-data = [
-]
+const keywords = {
+    "food": ['fruits', 'vegetables', 'apple', 'orange', 'bakery', 'grocery', 'meat', 'sugar'],
+    "countries": ['UK', 'population', 'Europe', 'kingdom', 'asia', 'america', 'africa'],
+    "animals": ['dog', 'cat', 'pet', 'wild', 'domestic', 'wolf', 'bear'],
+    "books": ['author', 'hardback', 'paperback', 'amazon', 'kindle', 'fiction', 'biography'],
+    "movies": ['theatre', 'horror', 'genre', 'action', 'romance', 'fantasy']
+}
+
+const data = {
+    "food": [
+        {
+            result: 'Pancake recipes',
+            link: 'https://bbcfood.co.uk/'
+        },
+        {
+            result: 'Another link',
+            link: '/'
+        },
+        {
+            result: 'Another link',
+            link: '/'
+        },
+    ],
+    
+    "countries": [
+        {
+            result: 'About Europe',
+            link: 'https://wikipedia.co.uk/'
+        },
+        {
+            result: 'Another link',
+            link: '/'
+        },
+        {
+            result: 'Another link',
+            link: '/'
+        },
+    ]
+}
+
+module.exports = {keywords, data}
