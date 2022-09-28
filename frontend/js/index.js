09273c883bfdac9cf3fa906e17254f8ef922e5bb
Tca0 <taher.khtieb@gmail.com>
2022-09-28 16:27:40 +0100

    fetching data from api adding an event listener

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,5 +1,25 @@
-const ip = 'localhost:3000'
+const ip = 'http://localhost:3000'
 
-// const searchInput = document.getElementById('searchInput')
-// const resultsBtn = document.getElementById('results10')
+const searchInput = document.getElementById('searchInput')
+const resultsBtn = document.getElementById('apiResults')
 // const resultsBtn2 = document.getElementById('results1')
+const resultsContainer = document.querySelector('.results')
+
+console.log(searchInput.value);
+
+// fetch data from api
+const getSearchResults = async (str) => {
+    const rawData = await fetch(`${ip}/search/${str}`)
+    // console.log(rawData);
+    const res = await rawData.json()
+    console.log(res);
+    
+}
+
+
+
+resultsBtn.addEventListener('click', () => {
+    console.log(searchInput.value);
+    const searchFor = searchInput.value;
+    getSearchResults(searchFor)
+})
