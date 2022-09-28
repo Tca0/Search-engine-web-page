e4b40351b4c7aedecc88c0d46abe50b29c444881
Tca0 <taher.khtieb@gmail.com>
2022-09-28 16:02:19 +0100

    update server to get results and send back a response

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const app = express()
 let cors = require('cors')
 app.use(cors())
 const {searchByKeyWords, compareTitle} = require('./helpers')
+const db = require('./data/dataBase')
 
 app.get('/', (req,res) => {
     console.log("hello app from app");
@@ -13,14 +14,25 @@ app.get('/', (req,res) => {
 app.get('/search/:str', (req, res) => {
     const searchStr = req.params.str
     console.log(`***** main app ****\n hitting sreach for: \n ${searchStr}`);
-    const results = searchByKeyWords(searchStr)
-    console.log("results from app");
-    console.log(results);
-
-    const arr = compareTitle(searchStr)
-    //console.log(arr)
-
-    res.end()
+    // search by title key words
+    const titleMatchs = compareTitle(searchStr)
+    console.log("results by title");
+    console.log(titleMatchs)
+    // search by tags key words
+    const tagMatchs = searchByKeyWords(searchStr)
+    console.log("results by tags");
+    console.log(tagMatchs);
+    let results = []
+    titleMatchs.forEach(ele => {
+        console.log(ele.index);
+        results.push(db[ele.index])
+    })
+    tagMatchs.forEach(ele => {
+        console.log(ele.index);
+        results.push(db[ele.index])
+    })
+    console.log(`The final response is: \n ${results}`);
+    res.status(202).send(results)
 })
 
 module.exports = app
