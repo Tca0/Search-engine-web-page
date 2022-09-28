91dda014a207e4e9174c7781895b0e5e3b0e9078
Ami <ami1996_tm@live.co.uk>
2022-09-28 11:04:00 +0100

    Added initial styles to homepage elements

---
+++ frontend/js/index.js
@@ -0,0 +1,5 @@
+const ip = 'localhost:3000'
+
+// const searchInput = document.getElementById('searchInput')
+// const resultsBtn = document.getElementById('results10')
+// const resultsBtn2 = document.getElementById('results1')
