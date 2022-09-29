128a4283705acf8dc9c9a9d513d57f329c475d3b
Tca0 <taher.khtieb@gmail.com>
2022-09-29 10:12:14 +0100

    changing searchByKeywords fun from includes to comparing operator

--- backend/helpers.js
+++ backend/helpers.js
@@ -4,27 +4,21 @@ const db = require('./data/database')
 let matchingSearchByWords = []
 function searchByKeyWords(searchStr) {
     matchingSearchByWords = []
-    // console.log(`dataBase :`);
-    // console.log(db);
     console.log("----- Search by tags function -----\n");
     // loop through all data indexes
     // compare the tags array with the searchStr words
     // if searchStr includes one of the tages then increase numberOfMatches 
     db.forEach((element, index) => {
+        // split the search sentence and compare it with tags
+        const words = searchStr.split(" ")
         let numberOfMatches = 0
-        // console.log(`element in data: \n`);
-        // console.log(element);
-        // console.log(element.tags);
         element.tags.forEach(tag => {
-            // console.log(`tag in element: ${tag}`);
-            if(searchStr.includes(tag)) {
-                console.log("matchimng in a tag in element", element);
-                console.log(searchStr);
-                console.log(`tag in element: ${tag}`);
-                numberOfMatches++
-            }
+            words.forEach(word => {
+                if(tag == word.toLowerCase()) {
+                    numberOfMatches++
+                }
+            })
         })
-        // console.log(`Number of matchs with the element in index ${db.indexOf(element)} is : ${numberOfMatches}`);
         // if there is a keywords matching then pushed to matchingSearchByWords array
         if(numberOfMatches>0) {
             matchingSearchByWords.push({
@@ -33,11 +27,7 @@ function searchByKeyWords(searchStr) {
             })
         }
     })
-    console.log("before sorting:");
-    console.log(matchingSearchByWords);
     matchingSearchByWords.sort((a,b) => b.matchs - a.matchs)
-    console.log("afetr sorting:");
-    console.log(matchingSearchByWords); 
     return matchingSearchByWords
 }
 
